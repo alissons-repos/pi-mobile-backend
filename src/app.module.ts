20d4e5e2ebbedb03f5a23e91fd31838d431a8015
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2023-10-26 19:40:06 -0300

    configurando a aplicação e fazendo sua conexão com o bando de dados resolved #12

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { ObjectsModule } from './objects/objects.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    ObjectsModule,
+    NotificationsModule,
+  ],
   controllers: [],
   providers: [],
 })
